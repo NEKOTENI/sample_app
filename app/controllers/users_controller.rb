815458977d8bb6b8089017cac51c19d0e9d23d78
NEKOTENI <riverfield.tk@gmail.com>
2016-07-17 01:10:50 +0000

    Finish login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,28 @@
 class UsersController < ApplicationController
-  # GET /user/1
+
   def show
-    # user = User.first # ローカル変数。このメソッド内でしか使えない。
-    #@user = User.first # インスタンス変数。これを使うとテンプレート（View）から呼び出せる。
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
   end
-  
-def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+
+  def create
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-  
-  private #これ以下はこのコントローラー内でしかアクセスできないメソッド一覧
+
+  private
 
     def user_params
-      # このパラメータしか登録を許さないよ
       params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
+                                   :password_confirmation)
     end
 end
